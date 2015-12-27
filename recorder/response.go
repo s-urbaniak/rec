5ce03fea4fe6861ec8381158ddd34b05719fcf81
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2015-12-27 18:41:18 +0100

    response: add docs

--- recorder/response.go
+++ recorder/response.go
@@ -2,16 +2,27 @@ package recorder
 
 import "fmt"
 
+// Response holds the response message from the recorder state machine.
+// It can be one of the Response* types.
 type Response interface{}
 
+// ResponseError indicates an error response.
 type ResponseError error
 
+// ResponseOK indicates a succesful operation.
 type ResponseOK struct{}
 
-func NewResponseErrorf(format string, a ...interface{}) Response {
+// ResponseLevel return the current known recording level.
+type ResponseLevel MsgLevel
+
+// NewResponseErrorf returns a new ResponseError
+// according to the given format specifier.
+func NewResponseErrorf(format string, a ...interface{}) ResponseError {
 	return fmt.Errorf(format, a...)
 }
 
-func NewResponseError(err error) Response {
+// NewResponseError returns a new ResponseError
+// based on the given error.
+func NewResponseError(err error) ResponseError {
 	return err
 }
