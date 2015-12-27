3c1c45e60fc7f6249ad430cc61f0b569262fd5ba
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2015-12-27 17:52:48 +0100

    main: stop recorder after 5 seconds

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"runtime"
+	"time"
 
 	"github.com/s-urbaniak/glib"
 	"github.com/s-urbaniak/rec/recorder"
@@ -19,6 +20,14 @@ func main() {
 		start := recorder.NewRequestStart()
 		recorder.Enqueue(start)
 		log.Printf("start response %T\n", <-start.ResponseChan)
+	}()
+
+	go func() {
+		time.Sleep(5 * time.Second)
+		log.Println("stopping")
+		stop := recorder.NewRequestStop()
+		recorder.Enqueue(stop)
+		log.Printf("stop response %T\n", <-stop.ResponseChan)
 	}()
 
 	glib.NewMainLoop(nil).Run()
